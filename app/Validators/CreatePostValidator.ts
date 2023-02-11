1dff17db94ae982f8332ab9e3e2f18a3cd339cbd
Kabirou ALASSANE <86885681+BlakvGhost@users.noreply.github.com>
2023-02-11 01:00:54 +0100

    Update CreatePostValidator.ts

--- app/Validators/CreatePostValidator.ts
+++ app/Validators/CreatePostValidator.ts
@@ -4,25 +4,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class CreatePostValidator {
   constructor(protected ctx: HttpContextContract) {}
 
-  /*
-   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
-   *
-   * For example:
-   * 1. The username must be of data type string. But then also, it should
-   *    not contain special characters or numbers.
-   *    ```
-   *     schema.string({}, [ rules.alpha() ])
-   *    ```
-   *
-   * 2. The email must be of data type string, formatted as a valid
-   *    email. But also, not used by any other user.
-   *    ```
-   *     schema.string({}, [
-   *       rules.email(),
-   *       rules.unique({ table: 'users', column: 'email' }),
-   *     ])
-   *    ```
-   */
+  
   public schema = schema.create({
     title: schema.string(),
     content: schema.string(),
